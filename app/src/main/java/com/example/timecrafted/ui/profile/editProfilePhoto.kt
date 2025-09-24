7b9394ff57abfb3f7f6c38f6a138ebeecc591cd8
rudanisoham <rudanisoham@gmail.com>
2025-09-24 11:18:59 +0530

    change

---
+++ app/src/main/java/com/example/timecrafted/ui/profile/editProfilePhoto.kt
@@ -0,0 +1,17 @@
+package com.example.timecrafted.ui.profile
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.timecrafted.R
+
+class editProfilePhoto : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_edit_profile_photo)
+
+    }
+}
